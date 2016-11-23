3c04f315606d5206a20938aecf9286b8dca537a2
jinxiu.qi <suiterchik@gmail.com>
2016-11-23 17:15:50 +0800

    .

--- spyder/jd/config.py
+++ spyder/jd/config.py
@@ -2,7 +2,7 @@
 import ConfigParser
 
 cf = ConfigParser.ConfigParser()
-cf.read('../config.ini')
+cf.read('/home/suit/lab/spyder/config.ini')
 DATABASE_URI = cf.get('jd', 'sql_conn')
 url = cf.get('jd', 'url')
 max_iter = int(cf.get('jd', 'max_iter'))
