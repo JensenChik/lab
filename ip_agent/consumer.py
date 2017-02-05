34c6aeb8fb25eddc225b2b0981946daae576f5f2
jinxiu.qi <suiterchik@gmail.com>
2017-02-05 18:40:43 +0800

    将batch_size改为成员变量

--- ip_agent/consumer.py
+++ ip_agent/consumer.py
@@ -5,11 +5,11 @@ from config import DBSession
 
 class Consumer:
     def __init__(self):
-        self.batch_size = 20
+        self._batch_size = 20
 
     def gen_proxies(self):
         session = DBSession()
-        valid_ip = session.query(IP).filter(IP.rank != None).order_by(IP.rank).limit(self.batch_size).all()
+        valid_ip = session.query(IP).filter(IP.rank != None).order_by(IP.rank).limit(self._batch_size).all()
         proxies = None if valid_ip == [] else choice(valid_ip).to_proxy()
         session.close()
         return proxies
