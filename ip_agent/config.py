01f4ecdf623a8a2352aceebf346281932a47d34a
jinxiu.qi <suiterchik@gmail.com>
2017-02-05 18:39:50 +0800

    修改配置文件路径

--- ip_agent/config.py
+++ ip_agent/config.py
@@ -1,11 +1,10 @@
 import ConfigParser
 import os
-import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 cf = ConfigParser.ConfigParser()
-cf.read(os.path.join(sys.path[0], 'config.ini'))
+cf.read(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'config.ini'))
 DATABASE_URI = cf.get('param', 'sql_conn')
 HEART_BEAT = int(cf.get('param', 'heart_beat'))
 POOL_SIZE = int(cf.get('param', 'pool_size'))
