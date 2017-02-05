5dede62b3730cee76eba9c88471cf9c080008738
jinxiu.qi <suiterchik@gmail.com>
2017-02-05 17:21:24 +0800

    代理ip消费者

---
+++ ip_agent/consumer.py
@@ -0,0 +1,15 @@
+from random import choice
+from model import IP
+from config import DBSession
+
+
+class Consumer:
+    def __init__(self):
+        self.batch_size = 20
+
+    def gen_proxies(self):
+        session = DBSession()
+        valid_ip = session.query(IP).filter(IP.rank != None).order_by(IP.rank).limit(self.batch_size).all()
+        proxies = None if valid_ip == [] else choice(valid_ip).to_proxy()
+        session.close()
+        return proxies
