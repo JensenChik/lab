42f573e47981d13ba717bde85ac8e15df131c311
jinxiu.qi <suiterchik@gmail.com>
2017-04-04 22:50:40 +0800

    添加西刺源

--- ipool/producer.py
+++ ipool/producer.py
@@ -36,7 +36,7 @@ class Producer:
     def __init__(self):
         self._source = [
             SixSix(), QuanMin(), CooBoBo(), Data5U(),
-            # XiCi(style='nt'), XiCi(style='nn'), XiCi(style='wt'),
+            XiCi(style='nt'), XiCi(style='nn'), XiCi(style='wt'),
             YunDaiLi(style=1), YunDaiLi(style=2), YunDaiLi(style=3), YunDaiLi(style=4),
             YunHai(style=1), YunHai(style=2), YunHai(style=3), YunHai(style=4),
         ]
