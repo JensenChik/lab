1612aed70a936364e552c9d94cd7809d91731d66
jinxiu.qi <suiterchik@gmail.com>
2017-04-05 13:09:04 +0800

    server发布

--- ipool/server.py
+++ ipool/server.py
@@ -6,9 +6,9 @@ sys.setdefaultencoding('utf-8')
 
 import json
 from random import choice
-from flask import Flask, g
+from flask import Flask
 from flask.ext.script import Manager, Server
-from config import DBSession
+from config import DBSession, host, port
 from model import IP
 
 app = Flask(__name__)
@@ -17,7 +17,7 @@ app = Flask(__name__)
 @app.route('/')
 def get():
     session = DBSession()
-    valid_ip = session.query(IP).filter(IP.speed != None).order_by(IP.speed).limit(10).all()
+    valid_ip = session.query(IP).all()
     proxies = None if valid_ip == [] else choice(valid_ip).to_proxy()
     session.close()
     return json.dumps(proxies) if proxies is not None else 'None'
@@ -25,6 +25,6 @@ def get():
 
 if __name__ == "__main__":
     manager = Manager(app)
-    server = Server(host='127.0.0.1', port=13579)
+    server = Server(host=host, port=port)
     manager.add_command("runserver", server)
     manager.run()
