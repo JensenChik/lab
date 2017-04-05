2b9e830a1a0b16d723cf1e7f04f4e9381052652e
jinxiu.qi <suiterchik@gmail.com>
2017-04-05 23:01:24 +0800

    修改为并发爬取并保证数据库写入一致性

--- spyder/jd/run.py
+++ spyder/jd/run.py
@@ -5,75 +5,64 @@ import json
 import time
 from datetime import datetime
 import os
+import multiprocessing
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir, os.path.pardir)))
-from spyder.config import DBSession, JD_max_iter, JD_url, logger, ip_pool
+from spyder.config import DBSession, JD_max_iter, JD_url, logger, HEADER, get_proxy
+from spyder.config import return_none_when_exception, repeat_while_return_none
 from spyder.model import JDWare as Ware
 
 
-def get(p):
-    param = {'_format_': 'json', 'stock': 1, 'page': p, 'keyword': keyword}
-    try:
-        req = requests.post(JD_url, data=param, proxies=ip_pool.gen_proxies(), timeout=10)
-        respond = json.loads(json.loads(req.content)['value']) if req.status_code == 200 else None
-    except Exception:
-        respond = None
+def get(param):
     time.sleep(1)
-    return respond
+    page, keyword = param
+    param = {'_format_': 'json', 'stock': 1, 'page': page, 'keyword': keyword}
+    proxy = get_proxy()
+    req = requests.post(JD_url, data=param, proxies=proxy, headers=HEADER, timeout=5)
+    if req.status_code != 200: raise Exception('error return code')
+    info = json.loads(json.loads(req.content).get('value'))
+    print 'get ware {} in page {} by {} done'.format(keyword, page, proxy)
+    time.sleep(4)
+    return [Ware(key_word=keyword, json=json.dumps(ware, ensure_ascii=False), create_time=datetime.now())
+            for ware in info.get('wareList').get('wareList') or []]
+
+
+def check_ware_count(keyword):
+    param = {'_format_': 'json', 'stock': 1, 'page': 1, 'keyword': keyword}
+    return json.loads(
+        json.loads(
+            requests.post(JD_url, data=param, headers=HEADER, timeout=5).content
+        ).get('value')
+    ).get('wareList').get('wareCount')
 
 
 if __name__ == '__main__':
     keyword = sys.argv[1]
     page = 0 if len(sys.argv) == 2 else sys.argv[2]
-    log = "抓取 {} 第 {} 页完毕, 当前页商品个数{}, 总商品个数{}, 累计已抓{}"
-    ware_count = 0
-    for i in range(JD_max_iter):
-        session = DBSession()
-        page += 1
-        info = get(page)
-        while info is None:
-            info = get(page)
-        ware_list = info['wareList']['wareList']
-        total_count = info['wareList']['wareCount']
-        ware_count += len(ware_list)
-        logger.warning(log.format(keyword, page, len(ware_list), total_count, ware_count))
-        for ware in ware_list:
-            w = Ware(
-                key_word=keyword,
-                ware_id=str(ware['wareId']),
-                name=str(ware['wname']),
-                price=str(ware['jdPrice']),
-                diff_mobile_price=str(ware['diffMobilePrice']),
-                is_self=str(ware['self']),
-                shop_name=str(ware['shopName']),
-                stock_state=str(ware['stockState']),
-                available_in_store=str(ware['availableInStore']),
-                pre_sale=str(ware['preSale']),
-                total_count=str(ware['totalCount']),
-                good=str(ware.get('good')),
-                sams_tag=None,
-                sams_price=None,
-                eBook_flag=str(ware['eBookFlag']),
-                can_free_read=str(ware['canFreeRead']),
-                promotion_flag=None,
-                m_promotion_id=str(ware['mPromotionId']),
-                is_booking=str(ware['isBooking']),
-                dis_price=None,
-                cid1=None,
-                cid2=None,
-                cat_id=None,
-                up_to_saving=str(ware['upToSaving']),
-                eche=str(ware['eche']),
-                author=str(ware['author']),
-                priority=str(ware['priority']),
-                international=str(ware['international']),
-                stock_quantity=str(ware['stockQuantity']),
-                adword=str(ware['adword']),
-                lowest_buy=str(ware['lowestBuy']),
-                create_time=datetime.now()
+    batch_size = 25
+    ware_list = []
+    ware_count = check_ware_count(keyword)
+    print 'start to get {} wares from JD'.format(ware_count)
+    for batch in range(JD_max_iter / batch_size):
+        pool = multiprocessing.Pool(processes=batch_size)
+        ware_list += reduce(
+            lambda x, y: x + y,
+            pool.map(
+                repeat_while_return_none(return_none_when_exception(get)),
+                zip(range(batch * batch_size, (batch + 1) * batch_size), [keyword] * batch_size)
             )
-            session.add(w)
-        session.commit()
-        session.close()
-        if ware_count >= int(total_count): break
-    print '抓取完毕'
+        )
+        pool.close()
+        pool.join()
+        print 'batch {} finish, acc ware count:{}'.format(batch, len(ware_list))
+        if len(ware_list) > ware_count: break
+    print 'get ware from JD finish'
+
+    print 'start to dump ware to mysql'
+    session = DBSession()
+    session.add_all(ware_list)
+    session.commit()
+    session.close()
+    print 'dump ware to mysql finish'
+
+    print 'all done'
