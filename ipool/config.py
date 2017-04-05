5a6377dfbe0a2744302eaae20af956ae30124801
jinxiu.qi <suiterchik@gmail.com>
2017-04-05 13:08:43 +0800

    更新配置变量

--- ipool/config.py
+++ ipool/config.py
@@ -2,19 +2,16 @@ import ConfigParser
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-import logging
-
 
 cf = ConfigParser.ConfigParser()
-cf.read(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'config.ini'))
-DATABASE_URI = cf.get('param', 'db_uri')
-POOL_URL = cf.get('param', 'pool_url')
+cf.read(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'ipool.ini'))
 
+DATABASE_URI = cf.get('db', 'uri')
 engine = create_engine(DATABASE_URI, pool_recycle=3600, encoding='utf-8')
 DBSession = sessionmaker(engine)
 
-logging.basicConfig()
-logging.getLogger('sqlalchemy.engine').setLevel(logging.WARNING)
+host = cf.get('ipoolserver', 'host')
+port = cf.get('ipoolserver', 'port')
 
 
 def return_none_when_exception(func):
