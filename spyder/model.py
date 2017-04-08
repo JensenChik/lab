f97a46c2c6ab5efc00dcc30244a732b3b6813541
jinxiu.qi <suiterchik@gmail.com>
2017-04-08 23:25:23 +0800

    keyword加索引

--- spyder/model.py
+++ spyder/model.py
@@ -7,7 +7,7 @@ BaseModel = declarative_base()
 class JDWare(BaseModel):
     __tablename__ = 'jd_ware'
     id = Column(Integer, primary_key=True)
-    key_word = Column(Text)
+    key_word = Column(Text, index=True)
     json = Column(Text)
     create_time = Column(DateTime)
 
