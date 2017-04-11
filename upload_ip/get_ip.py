e43c599820d7b49f33de594b779f035377225cdd
jinxiu.qi <suiterchik@gmail.com>
2017-04-11 23:54:31 +0800

    将上传ip设置为死循环

--- upload_ip/get_ip.py
+++ upload_ip/get_ip.py
@@ -5,26 +5,15 @@ import re
 import commands
 import datetime
 import time
-import sys
 
-hour_list = range(24)
-
-while len(hour_list) != 0:
-    current_hour = datetime.datetime.now().hour
-    if current_hour in hour_list:
-        try:
-            req = requests.get('http://m.baidu.com/s?word=ip')
-            html = BeautifulSoup(req.content, 'html.parser')
-            ip = html.find_all(text=re.compile(u'本机IP:'))[0].split(':')[-1]
-            print datetime.datetime.now(), ip
-            status, info = commands.getstatusoutput("ssh root@conj.space 'cd /home/env; python update_ip.py %s'" % ip)
-            if status != 0:
-                print status, info
-                sys.exit(404)
-            else:
-                hour_list.remove(current_hour)
-        except Exception, e:
-            print e
-    time.sleep(60*30)
-
-
+while True:
+    try:
+        req = requests.get('http://m.baidu.com/s?word=ip')
+        html = BeautifulSoup(req.content, 'html.parser')
+        ip = html.find_all(text=re.compile(u'本机IP:'))[0].split(':')[-1]
+        print datetime.datetime.now(), ip
+        status, info = commands.getstatusoutput("ssh root@conj.space 'cd /home/env; python update_ip.py %s'" % ip)
+        if status != 0: raise Exception("更新远程IP信息失败")
+    except Exception, e:
+        print e
+    time.sleep(3600)
